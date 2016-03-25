eb0190ec6ad6b6e2c5ab76cf20a20de98238d42e
Victor Novy <novyvictor@gmail.com>
2016-03-24 21:29:13 -0300

    Integration with bootstrap

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -2,23 +2,29 @@ if (Meteor.isClient) {
     console.log("Cliente");
     var img_data = [
         {
+            "name": "Fruits",
             "img_src": "fruits.jpg",
             "img_alt": "Some Fruits"
         },
         {
+            "name": "Banana",
             "img_src": "banana.jpg",
             "img_alt": "A Banana"
         },
         {
+            "name": "Apple",
             "img_src": "apple.jpg",
             "img_alt": "An apple"
         },
         {
+            "name": "Orange",
             "img_src": "orange.jpg",
             "img_alt": "An orange"
         }
     ];
     Template.images.helpers({images: img_data});
+
+
 }
 
 if (Meteor.isServer) {
