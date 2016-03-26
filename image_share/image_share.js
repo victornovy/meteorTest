5e925e12af667c2cd7532f69d95291297bbec81a
Victor Novy <novyvictor@gmail.com>
2016-03-25 23:34:28 -0300

    - init mongodb(insert, delete and find) - bootstrap button

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -1,39 +1,16 @@
+Images = new Mongo.Collection("images");
+
 if (Meteor.isClient) {
     console.log("Cliente");
-    var img_data = [
-        {
-            "name": "Fruits",
-            "img_src": "fruits.jpg",
-            "img_alt": "Some Fruits"
-        },
-        {
-            "name": "Banana",
-            "img_src": "banana.jpg",
-            "img_alt": "A Banana"
-        },
-        {
-            "name": "Apple",
-            "img_src": "apple.jpg",
-            "img_alt": "An apple"
-        },
-        {
-            "name": "Orange",
-            "img_src": "orange.jpg",
-            "img_alt": "An orange"
-        }
-    ];
-    Template.images.helpers({images: img_data});
 
-    Template.images.events(
-        {
-            'click .js-images': function(event) {
-                console.log('Event test in Meteor');
-                $(event.target).css("width", "50px");
-            }
-        }
-    );
-}
+    Template.images.helpers({images: Images.find()});
 
-if (Meteor.isServer) {
-    console.log("Server");
+    Template.images.events({
+        'click .js-del-image': function(event) {
+            console.log("Id image removed " + this._id);
+            $("#" + this._id).hide("slow", function() {
+                Images.remove({"_id": this._id})
+            });
+        }
+    });
 }
