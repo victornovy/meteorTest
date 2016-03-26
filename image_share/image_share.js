d5a49ffe7542a2375b6f9af15eadeef37366928c
Victor Novy <novyvictor@gmail.com>
2016-03-26 00:36:01 -0300

    -Created rating star and changed the images rate in mongodb -Sort the informations

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -3,7 +3,10 @@ Images = new Mongo.Collection("images");
 if (Meteor.isClient) {
     console.log("Cliente");
 
-    Template.images.helpers({images: Images.find()});
+    //.find('filter', sort)
+    Template.images.helpers({
+        images: Images.find({}, {sort: {rating: -1}})
+    });
 
     Template.images.events({
         'click .js-del-image': function(event) {
@@ -11,6 +14,14 @@ if (Meteor.isClient) {
             $("#" + this._id).hide("slow", function() {
                 Images.remove({"_id": this._id})
             });
+        },
+        'click .js-rate-image': function(event) {
+            var rating = $(event.currentTarget).data("userrating");
+            var image_id = this.id;
+
+            console.log(rating);
+            console.log(image_id);
+            Images.update({_id: image_id}, {$set: {rating: rating}});
         }
     });
 }
