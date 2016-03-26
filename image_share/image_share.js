c3079d3ba7a05b0aafaee144db958cadfe7f719c
Victor Novy <novyvictor@gmail.com>
2016-03-26 13:01:18 -0300

    -Fixed remove image -Created a form modal with bootstrap

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -3,25 +3,45 @@ Images = new Mongo.Collection("images");
 if (Meteor.isClient) {
     console.log("Cliente");
 
-    //.find('filter', sort)
+    //.find('filter', 'sort')
     Template.images.helpers({
-        images: Images.find({}, {sort: {rating: -1}})
+        images: Images.find({}, {sort: {createdOn: -1, rating: -1}})
     });
 
     Template.images.events({
         'click .js-del-image': function(event) {
             console.log("Id image removed " + this._id);
-            $("#" + this._id).hide("slow", function() {
-                Images.remove({"_id": this._id})
+            var image_id = this._id;
+            $("#" + image_id).hide("slow", function() {
+                Images.remove({"_id": image_id})
             });
         },
         'click .js-rate-image': function(event) {
             var rating = $(event.currentTarget).data("userrating");
             var image_id = this.id;
 
-            console.log(rating);
-            console.log(image_id);
             Images.update({_id: image_id}, {$set: {rating: rating}});
+        },
+        'click .js-show-image-form': function(event) {
+            $("#image_form_modal").modal("show");
+        }
+    });
+
+    Template.images_add_form.events({
+        'submit .js-add-image': function(event) {
+            var img_src, img_alt;
+            img_src = event.currentTarget.img_src.value;
+            img_alt = event.currentTarget.img_alt.value;
+
+            Images.insert({
+                "img_src": img_src,
+                "img_alt": img_alt,
+                "createdOn": new Date()
+            });
+
+            $("#image_form_modal").modal("hide");
+
+            return false;
         }
     });
 }
--- image_share/startup.js
+++ image_share/startup.js
@@ -3,34 +3,26 @@ if (Meteor.isServer) {
 
     Meteor.startup(function() {
         if (Images.find().count() === 0) {
-            Images.insert(
-                {
-                    "name": "Fruits",
-                    "img_src": "fruits.jpg",
-                    "img_alt": "Some Fruits"
-                }
-            );
-            Images.insert(
-                {
-                    "name": "Banana",
-                    "img_src": "banana.jpg",
-                    "img_alt": "A Banana"
-                }
-            );
-            Images.insert(
-                {
-                    "name": "Apple",
-                    "img_src": "apple.jpg",
-                    "img_alt": "An apple"
-                }
-            );
-            Images.insert(
-                {
-                    "name": "Orange",
-                    "img_src": "orange.jpg",
-                    "img_alt": "An orange"
-                }
-            );
+            Images.insert({
+                "name": "Fruits",
+                "img_src": "fruits.jpg",
+                "img_alt": "Some Fruits"
+            });
+            Images.insert({
+                "name": "Banana",
+                "img_src": "banana.jpg",
+                "img_alt": "A Banana"
+            });
+            Images.insert({
+                "name": "Apple",
+                "img_src": "apple.jpg",
+                "img_alt": "An apple"
+            });
+            Images.insert({
+                "name": "Orange",
+                "img_src": "orange.jpg",
+                "img_alt": "An orange"
+            });
         }
     });
 }
