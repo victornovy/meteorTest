f1735649e46e762e139cb34de1943cff252d1fdc
Victor Novy <novyvictor@gmail.com>
2016-04-02 00:39:27 -0300

    -tidied project structure

---
+++ image_share/server/server.js
@@ -0,0 +1,27 @@
+console.log("###### Server ######");
+
+//Meteor Startup
+Meteor.startup(function() {
+    if (Images.find().count() === 0) {
+        Images.insert({
+            "name": "Fruits",
+            "img_src": "fruits.jpg",
+            "img_alt": "Some Fruits"
+        });
+        Images.insert({
+            "name": "Banana",
+            "img_src": "banana.jpg",
+            "img_alt": "A Banana"
+        });
+        Images.insert({
+            "name": "Apple",
+            "img_src": "apple.jpg",
+            "img_alt": "An apple"
+        });
+        Images.insert({
+            "name": "Orange",
+            "img_src": "orange.jpg",
+            "img_alt": "An orange"
+        });
+    }
+});
